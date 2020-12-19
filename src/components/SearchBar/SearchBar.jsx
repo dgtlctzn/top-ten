de1e611a29af427deb4b20fde6867c0fd11cd976
Joseph Perry <josephperry720@gmail.com>
2020-12-19 10:44:01 -0500

    added focus to search bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 
 const SearchBar = ({ page, search, handleSearchSubmit, handleSearchChange }) => {
+
+  const searchEl = useRef(null);
+
+  useEffect(() => {
+    if(searchEl) {
+      searchEl.current.focus();
+    }
+  }, [])
+
   return (
     <form onSubmit={handleSearchSubmit} className="text-center">
       <div className="mb-3">
@@ -14,6 +23,7 @@ const SearchBar = ({ page, search, handleSearchSubmit, handleSearchChange }) => 
           aria-describedby="emailHelp"
           value={search}
           onChange={handleSearchChange}
+          ref={searchEl}
         />
       </div>
 
