a0c4e206b15e63b6018475c27978e29094b74124
Joseph Perry <josephperry720@gmail.com>
2020-12-19 11:28:32 -0500

    facebook share added

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -2,8 +2,38 @@ import React from "react";
 import Nav from "../../components/Nav/Nav";
 import "./Home.css";
 import { Link } from "react-router-dom";
+import { FacebookShareButton, FacebookIcon } from "react-share";
+import { useSelector } from "react-redux";
 
 const Home = () => {
+  const topTenAlbums = useSelector((state) => state.savedAlbumsReducer);
+  const topTenMovies = useSelector((state) => state.savedMoviesReducer);
+  const topTenBooks = useSelector((state) => state.savedBooksReducer);
+
+  // formats top ten lists into single shareable string
+  const stringList = (albums, movies, books) => {
+    const categories = [albums, movies, books];
+    let topTenString = "";
+    for (const category of categories) {
+      if (category.length) {
+        category.forEach((item, index) => {
+          if (!index) {
+            topTenString +=
+              `My Top ${category.length} ${item.type}s:` +
+              "\n" +
+              `${index + 1}. ${item.name}` +
+              "\n";
+          } else if (index === category.length - 1)
+            topTenString += `${index + 1}. ${item.name}` + "\n" + "\n";
+          else {
+            topTenString += `${index + 1}. ${item.name}` + "\n";
+          }
+        });
+      }
+    }
+    return topTenString;
+  };
+
   return (
     <div>
       <Nav />
@@ -13,9 +43,29 @@ const Home = () => {
         </div>
         <div className="row">
           <h2 className="col-sm-12 text-center">
-            A site to rank your favorite <Link className="link" to="/movies">Movies</Link>,{" "}
-            <Link className="link" to="/albums">Albums</Link>, and <Link className="link" to="/books">Books</Link>
+            A site to rank your favorite{" "}
+            <Link className="link" to="/movies">
+              Movies
+            </Link>
+            ,{" "}
+            <Link className="link" to="/albums">
+              Albums
+            </Link>
+            , and{" "}
+            <Link className="link" to="/books">
+              Books
+            </Link>
           </h2>
+        </div>
+        <div className="row">
+          <div className="col-sm-12 text-center">
+            <FacebookShareButton
+              url="google.com"
+              quote={stringList(topTenAlbums, topTenMovies, topTenBooks)}
+            >
+              <FacebookIcon />
+            </FacebookShareButton>
+          </div>
         </div>
       </div>
     </div>
