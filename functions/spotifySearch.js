4b5e39026144f8ff3b224a605d2c9937fc725f4e
Joseph Perry <josephperry720@gmail.com>
2021-01-08 10:41:38 -0500

    console logged some things

--- functions/spotifySearch.js
+++ functions/spotifySearch.js
@@ -2,7 +2,10 @@ const axios = require("axios");
 const qs = require("qs");
 
 exports.handler = async function (event, context) {
+  console.log(event.body);
   const { search, token } = qs.parse(event.body);
+  console.log(token);
+  console.log(search);
   const { data } = await axios({
     method: "GET",
     url: `https://api.spotify.com/v1/search?q=${search}&type=album`,
