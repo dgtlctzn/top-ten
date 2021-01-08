e906cb5ec0887366218126eeb3d24ad58fb368df
Joseph Perry <josephperry720@gmail.com>
2021-01-08 10:49:52 -0500

    added try catch to async block

--- functions/spotifySearch.js
+++ functions/spotifySearch.js
@@ -1,18 +1,23 @@
 const axios = require("axios");
-const qs = require("qs");
 
 exports.handler = async function (event, context) {
-  console.log(event.body);
-  const { search, token } = JSON.parse(event.body);
-  console.log(token);
-  console.log(search);
-  const { data } = await axios({
-    method: "GET",
-    url: `https://api.spotify.com/v1/search?q=${search}&type=album`,
-    headers: { Authorization: `Bearer ${token}` },
-  });
-  return {
+  try {
+    const { search, token } = JSON.parse(event.body);
+
+    const { data } = await axios({
+      method: "GET",
+      url: `https://api.spotify.com/v1/search?q=${search}&type=album`,
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    return {
       statusCode: 200,
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: null,
+    };
   }
 };
